e56ca789647227e7c2dd6bb0097403f402e0276f
hisho <hisho.web@gmail.com>
2023-06-28 21:19:09 +0900

    📝 create section-01

--- src/app/(root)/_component/section-01.tsx
+++ src/app/(root)/_component/section-01.tsx
@@ -1,5 +1,11 @@
 import { NewSection } from '@/src/app/(root)/_template/new-section'
 
 export const Section01 = () => {
-  return <NewSection heading={'※過激な内容が含まれます'} />
+  return (
+    <NewSection
+      heading={'⚠一部過激な内容が含まれます⚠'}
+      headingClassName={'text-red-500'}
+      headingLevel={'h4'}
+    />
+  )
 }

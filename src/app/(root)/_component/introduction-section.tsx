03899859065b0cc8e3589caef4b31e9d99ba7a9f
hisho <hisho.web@gmail.com>
2023-06-30 03:51:59 +0900

    📝 update

--- src/app/(root)/_component/introduction-section.tsx
+++ src/app/(root)/_component/introduction-section.tsx
@@ -1,5 +1,14 @@
 import { NewSection } from '@/src/app/(root)/_template/new-section'
 
 export const IntroductionSection = () => {
-  return <NewSection heading={'tailwindcssのススメ'} />
+  return (
+    <NewSection
+      heading={
+        <>
+          <strong className={'text-sky-400'}>スタイリングの風</strong> :
+          <span className={'text-gray-300'}>クラス名の断捨離へ</span>
+        </>
+      }
+    />
+  )
 }
--- src/app/(root)/_component/section-02-heading.tsx
+++ src/app/(root)/_component/section-02-heading.tsx
@@ -1,5 +1,17 @@
 import { NewSection } from '@/src/app/(root)/_template/new-section'
+import { TailwindcssIcon } from '@/src/icon/tailwindcss'
 
 export const Section02Heading = () => {
-  return <NewSection heading={'tailwindcssとはなにか'} headingLevel={'h2'} />
+  return (
+    <NewSection
+      heading={
+        <span className={'flex items-center justify-center'}>
+          <TailwindcssIcon className={'w-full max-w-xl'} />
+          <span className={'shrink-0'}>とはなにか</span>
+        </span>
+      }
+      headingClassName={'w-full text-center'}
+      headingLevel={'h2'}
+    />
+  )
 }
